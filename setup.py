db7c15d95cc419c73318cbc04fac976f12d9903a
Chris Strutton <chris@rodandfly.ca>
2020-09-22 12:20:36 -0400

    update install procedure

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='A collection of python scripts to collect production data from industrial PLC\'s into a mysql database',
     packages=find_packages(include=['prodmon', 'prodmon.*']),
     install_requires=[
-        'pylogix~ = 0.7.0',
+        'pylogix~=0.7.0',
         'PyYAML~=5.3.1',
         'Flask~=1.1.2',
         'mysql-connector-python~=8.0.21'

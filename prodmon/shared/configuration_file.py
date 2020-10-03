979c83d18b3302d1e816c8b5e75dc4cd490201a0
Chris Strutton <chris@rodandfly.ca>
2020-10-03 11:50:04 -0400

    update configuration location to /etc with cmd-line override

--- prodmon/shared/configuration_file.py
+++ prodmon/shared/configuration_file.py
@@ -3,20 +3,19 @@ import sys
 import yaml
 
 
-def config_default(dict, key, default):
-    if not key in dict:
-        dict[key] = default
+def config_default(config_dict, key, default):
+    if key not in config_dict:
+        config_dict[key] = default
 
 
 def get_config(config_key):
-
-    if len(sys.argv) < 2:
-        raise ValueError("Need config file")
-
-    if not os.path.isfile(sys.argv[1]):
-        raise ValueError("Config file does not exist")
-
-    with open(sys.argv[1], 'r') as file:
+    if os.path.isfile(sys.argv[1]):
+        config_path = sys.argv[1]
+    elif os.path.isfile(f'/etc/prodmon/{config_key}.yml'):
+        config_path = f'/etc/prodmon/{config_key}.yml'
+    else:
+        raise ValueError('Config file not found!')
+    with open(config_path, 'r') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
         config = config[config_key]
 

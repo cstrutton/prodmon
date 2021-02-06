d50cb0892d89b5ffcd220b2ac6d3fa1cc1d4fc35
Chris Strutton <chris@rodandfly.ca>
2021-02-06 10:14:47 -0500

    configure script hello world

--- scripts/setplcip.sh
+++ scripts/setplcip.sh
@@ -1,3 +1,5 @@
-echo Enter IP Address
-read newIP
-echo It\'s nice to meet you $newIP
+read -p 'PLC IP Address: ' PLC_IP
+read -p 'PLC Netmask: ' PLC_NETMASK
+echo It\'s nice to meet you $PLC_IP $PLC_NETMASK
+
+# connmanctl config ethernet_de3c0bf795c2_cable --ipv4 manual $(PLC_IP) $(PLC_NETMASK)

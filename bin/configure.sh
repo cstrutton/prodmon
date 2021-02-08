c89362f19101584b5fd7bbe97cd1e7e4ae550ed0
Chris Strutton <chris@rodandfly.ca>
2021-02-08 17:33:09 -0500

    cleaned up configure.sh, remaned bin dir

--- bin/configure.sh
+++ bin/configure.sh
@@ -9,15 +9,14 @@ fi
 print_menu() {
   local MENU=("$@")
   for index in "${!MENU[@]}"; do
-    printf "%s) %s\n" $(( $index+1 )) "${MENU[$index]}"
+    printf "%s) %s\n" $(( index+1 )) "${MENU[$index]}"
   done
   printf "Enter to continue:"
-  read choice
+  read -r -n 1 choice
   return $(( choice ))
 }
 
 areyousure() {
-  local response=0
   read -p "$*" -n 1 -r
   echo
   if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -27,56 +26,61 @@ areyousure() {
 }
 
 set_plc_network() {
+  local IP NETMASK
+
   printf "\n\nConfigure PLC Network\n"
 
-  local IP=$(ip addr show eth1 |\
-           grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" |\
-           grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-  read -e -i "$IP" -p "PLC IP Address: " IP
+  IP=$(ip addr show eth1 |\
+       grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" |\
+       grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+  read -er -i "$IP" -p "PLC IP Address: " IP
 
-  local NETMASK=$(ifconfig eth1 | awk '/netmask/{split($4,a,":"); print a[1]}')
-  read -e -i "$NETMASK" -p 'PLC Netmask: ' NETMASK
+  NETMASK=$(ifconfig eth1 | awk '/netmask/{split($4,a,":"); print a[1]}')
+  read -er -i "$NETMASK" -p 'PLC Netmask: ' NETMASK
 
-  printf "Execute: connmanctl config ethernet_dead12345678_cable --ipv4 manual ${IP} ${NETMASK}\n"
+  printf "Execute: connmanctl config ethernet_dead12345678_cable --ipv4 manual %s %s \n" "$IP" "$NETMASK"
   if areyousure "Proceed?:[yN]"; then
-    connmanctl config ethernet_dead12345678_cable --ipv4 manual ${IP} ${NETMASK}
+    connmanctl config ethernet_dead12345678_cable --ipv4 manual "${IP}" "${NETMASK}"
     printf "Command returned: %d\n" $?
   fi
 }
 
 set_plant_network() {
+  local MAC SERVICE_NAME IP NETMASK GATEWAY NAMESERVERS
   printf "\n\nConfigure Plant Network\n"
 
-  local MAC=$(cat /sys/class/net/eth2/address | tr -d ':')
-  local SERVICE_NAME="ethernet_${MAC}_cable"
+  MAC=$(tr -d ':' < /sys/class/net/eth2/address)
+  SERVICE_NAME="ethernet_${MAC}_cable"
 
-  local IP=$(ip addr show eth2 |\
+  IP=$(ip addr show eth2 |\
              grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" |\
              grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-  read -e -i "$IP" -p 'PLANT IP Address: ' IP
+  read -er -i "$IP" -p 'PLANT IP Address: ' IP
 
-  local NETMASK=$(ifconfig eth2 |\
-                  awk '/netmask/{split($4,a,":"); print a[1]}')
-  read -e -i "$NETMASK" -p 'Netmask: ' NETMASK
+  NETMASK=$(ifconfig eth2 | awk '/netmask/{split($4,a,":"); print a[1]}')
+  read -er -i "$NETMASK" -p 'Netmask: ' NETMASK
 
-  local GATEWAY=$(route -n |\
+  GATEWAY=$(route -n |\
                   grep 'UG[ \t]' |\
                   awk '{print $2}')
-  read -e -i "$GATEWAY" -p 'Gateway: ' GATEWAY
+  read -er -i "$GATEWAY" -p 'Gateway: ' GATEWAY
 
-  local NAMESERVERS=$(connmanctl services ${SERVICE_NAME} |\
+  NAMESERVERS=$(connmanctl services "${SERVICE_NAME}" |\
                       awk '/Nameservers.Configuration/ {print}' |\
-                      grep -Po '\[\K[^]]*' ${NAMESERVERS} | tr -d ',')
-  read -e -i "$NAMESERVERS" -p 'Nameservers: '
+                      grep -Po '\[\K[^]]*' "${NAMESERVERS}" | tr -d ',')
+  read -er -i "$NAMESERVERS" -p 'Nameservers: '
 
-  printf "Execute: connmanctl config ${SERVICE_NAME} --ipv4 manual ${IP} ${NETMASK} ${GATEWAY} --nameservers ${NAMESERVERS}\n"
+  printf "Execute: connmanctl config %s --ipv4 manual %s %s %s --nameservers %s\n" \
+         "${SERVICE_NAME}" "${IP}" "${NETMASK}" "${GATEWAY}" "${NAMESERVERS}"
   if areyousure "Proceed?:[yN]"; then
-    connmanctl config ${SERVICE_NAME} --ipv4 manual ${IP} ${NETMASK} ${GATEWAY} --nameservers ${NAMESERVERS}
+    connmanctl config "${SERVICE_NAME}" --ipv4 manual "${IP}" "${NETMASK}"\
+                      "${GATEWAY}" --nameservers "${NAMESERVERS}"
     printf "Command returned: %d\n" $?
   fi
 }
 
 set_configuration_files() {
+  local MENU FILES result FILENAME
   while true; do
     printf "\n\nInstall Configuration Files\n"
 
@@ -89,44 +93,44 @@ set_configuration_files() {
          local FILES=(./configs/*-collect.yml)
          print_menu "${FILES[@]}"
 
-         local result=$?
-         if ! [[ "$result" =~ "^[0-9]+$" ]]
-           then
-           (( result = 0 ))
-         fi
-
-         if ! [[ result == 0 ]]; then
-           local FILENAME=${FILES[((--result))]}
-           printf "Installing %s as /etc/prodmon/collect.config\n" $FILENAME
-           cp $FILENAME /etc/prodmon/collect.config
-           printf "Command returned: %d\n\n" $?
-
-           if (systemctl -q is-active collect.service); then
-             printf "Restarting the Collect service: "
-             systemctl restart collect
-             printf "Command returned: %d\n\n" $?
-           fi
-         fi ;;
-
-      2) printf "Load Post Configuration from:\n"
-         local FILES=(./configs/*-post.yml)
-         print_menu "${FILES[@]}"
-
-         local result=$?
-         if ! [[ "$result" =~ "^[0-9]+$" ]]
+         result=$?
+         if ! [[ "$result" =~ ^[0-9]+$ ]]
            then
            (( result = 0 ))
          fi
 
          if ! [[ $result == 0 ]]; then
-           local FILENAME=${FILES[((--result))]}
-           printf "Installing %s as /etc/prodmon/post.config :" $FILENAME
-           cp $FILENAME /etc/prodmon/post.config
-           printf "%d\n\n" $?
+           FILENAME=${FILES[((--result))]}
+           printf "Installing %s as /etc/prodmon/collect.config\n" "$FILENAME"
+           cp "$FILENAME" /etc/prodmon/collect.config
+           printf "Command returned: %d\n\n" $?
+
+           if (systemctl -q is-active collect.service); then
+             printf "Restarting the Collect service: "
+             systemctl restart collect
+             printf "Command returned: %d\n\n" "$?"
+           fi
+         fi ;;
+
+      2) printf "Load Post Configuration from:\n"
+         FILES=(./configs/*-post.yml)
+         print_menu "${FILES[@]}"
+
+         result=$?
+         if ! [[ "$result" =~ ^[0-9]+$ ]]
+           then
+           (( result = 0 ))
+         fi
+
+         if ! [[ $result == 0 ]]; then
+           FILENAME=${FILES[((--result))]}
+           printf "Installing %s as /etc/prodmon/post.config :" "$FILENAME"
+           cp "$FILENAME" /etc/prodmon/post.config
+           printf "%d\n\n" "$?"
            if (systemctl -q is-active post.service); then
              printf "Restarting the Post service: "
              systemctl restart post
-             printf "%d\n\n" $?
+             printf "%d\n\n" "$?"
            fi
          fi ;;
 
@@ -136,10 +140,10 @@ set_configuration_files() {
 }
 
 configure_service_files() {
+  local CHANGED MENU
   while true; do
     printf "\nConfigure Systemd Services\n"
-
-    local CHANGED=0
+    CHANGED=0
     MENU=("Install Collect Service Unit File"\
           "Enable/Disable Collect Service to run at boot"\
           "Install Post Service Unit File"\
@@ -186,20 +190,20 @@ configure_service_files() {
            (( CHANGED++ ))
          fi ;;
 
-      0) break ;;
+      0) if (( CHANGED > 0 )); then
+           printf "Running systemclt daemon-reload: "
+           systemctl daemon-reload
+           printf "%d\n\n" $?
+         fi
+         break ;;
 
     esac
-    if (( CHANGED > 0 )); then
-      printf "Running systemclt daemon-reload: "
-      systemctl daemon-reload
-      printf "%d\n\n" $?
-    fi
   done
 }
 
 force_reboot() {
   if areyousure "Reboot System Now? [yN]"; then
-    printf "Rebooting.... Wait 30 seconds before reconnecting"
+    printf "Rebooting.... Wait 10 seconds before attempting to reconnect"
     reboot now
   fi
 }

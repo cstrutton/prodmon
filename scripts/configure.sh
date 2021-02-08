20966b29fdc57e7dfc7519a4a872eb3955339fdb
DemoUser <debian@npi.localdomain>
2021-02-08 15:13:22 +0000

    cleaning up configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -6,75 +6,112 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-while true ; do
-  echo "1) Set PLC Network"
-  echo "2) Set PLANT Network"
-  echo "3) Set Config Files"
-  echo "4) Install Service Files"
-  echo "5) View Logs"
-  echo "Enter Exit"
+set_plc_network () {
+  echo "Set PLC Network"
+
+  local IP=$(ip addr show eth1 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*$
+  read -e -i "$IP" -p 'PLC IP Address: ' IP
+
+  local NETMASK=$(ifconfig eth1 | awk '/netmask/{split($4,a,":"); print a[1]}')
+  read -e -i "$NETMASK" -p 'PLC Netmask: ' NETMASK
+
+  echo "connmanctl config ethernet_dead12345678_cable --ipv4 manual ${IP} ${NETMASK}"
+}
+
+set_plant_network () {
+  echo "Set PLANT Network"
+
+  local MAC=$(cat /sys/class/net/eth2/address | tr -d ':')
+  local SERVICE_NAME="ethernet_${MAC}_cable"
+
+  local IP=$(ip addr show eth2 |\
+             grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" |\
+             grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+  read -e -i "$IP" -p 'PLANT IP Address: ' IP
+
+  local NETMASK=$(ifconfig eth2 |\
+                  awk '/netmask/{split($4,a,":"); print a[1]}')
+  read -e -i "$NETMASK" -p 'Netmask: ' NETMASK
+
+  local GATEWAY=$(route -n |\
+                  grep 'UG[ \t]' |\
+                  awk '{print $2}')
+  read -e -i "$GATEWAY" -p 'Gateway: ' GATEWAY
+
+  local NAMESERVERS=$(connmanctl services ${SERVICE_NAME} |\
+                      awk '/Nameservers.Configuration/ {print}' |\
+                      grep -Po '\[\K[^]]*' ${NAMESERVERS} | tr -d ',')
+  read -e -i "$NAMESERVERS" -p 'Nameservers: '
+
+  echo connmanctl config ${SERVICE_NAME} \
+                  --ipv4 manual ${IP} ${NETMASK} ${GATEWAY} \
+                  --nameservers ${NAMESERVERS}
+}
+
+select_config_files() {
+  echo "Select Config Files:"
+  echo "Collect Configuration:"
+  # set the prompt used by select, replacing "#?"
+  PS3="Use number to select a file or 'stop' to cancel: "
+  # allow the user to choose a file
+  select filename in ./configs/*-collect.yml; do
+    # leave the loop if the user says 'stop'
+    if [[ "$REPLY" == stop ]]; then break; fi
+    # complain if no file was selected, and loop to ask again
+    if [[ "$filename" == "" ]]; then
+      echo "'$REPLY' is not a valid choice"
+      continue
+    fi
+
+    # now we can use the selected file
+    echo "$filename installed"
+    # it'll ask for another unless we leave the loop
+    break
+  done
+
+  echo "Select a post configuration"
+  # set the prompt used by select, replacing "#?"
+  PS3="Use number to select a file or 'stop' to cancel: "
+  # allow the user to choose a file
+  select filename in ./configs/*-collect.yml; do
+    # leave the loop if the user says 'stop'
+    if [[ "$REPLY" == stop ]]; then break; fi
+    # complain if no file was selected, and loop to ask again
+    if [[ "$filename" == "" ]]; then
+      echo "'$REPLY' is not a valid choice"
+      continue
+    fi
+    # now we can use the selected file
+    echo "$filename installed"
+    # it'll ask for another unless we leave the loop
+    break
+  done
+}
+
+
+while true; do
+  clear
+  cat << _EOF_
+Please Select:
+
+1. Set PLC Network
+2. Set Plant Network
+3. Install Config Files
+4. Install Service Files
+0. Quit
+
+_EOF_
+
   read choice;
 
   case $choice in
-    1) echo "Set PLC Network"
-
-       PLC_IP=$(ip addr show eth1 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-       read -e -i "$PLC_IP" -p 'PLC IP Address: ' PLC_IP
-
-       PLC_NETMASK=$(ifconfig eth1 | awk '/netmask/{split($4,a,":"); print a[1]}')
-       read -e -i "$PLC_NETMASK" -p 'PLC Netmask: ' PLC_NETMASK
-
-       echo "connmanctl config ethernet_dead12345678_cable --ipv4 manual ${PLC_IP} ${PLC_NETMASK}"
+    1) set_plc_network()
        ;;
 
-    2) echo "Set PLANT Network"
-
-       PLANT_MAC=$(cat /sys/class/net/eth2/address | tr -d ':')
-       PLANT_SERVICE="ethernet_${PLANT_MAC}_cable"
-
-       PLANT_IP=$(ip addr show eth2 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-       read -e -i "$PLANT_IP" -p 'PLANT IP Address: ' PLANT_IP
-
-       PLANT_NETMASK=$(ifconfig eth2 | awk '/netmask/{split($4,a,":"); print a[1]}')
-       read -e -i "$PLANT_NETMASK" -p 'Netmask: ' PLANT_NETMASK
-
-       PLANT_GATEWAY=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
-       read -e -i "$PLANT_GATEWAY" -p 'Gateway: ' PLANT_GATEWAY
-
-       PLANT_NAMESERVERS=$(connmanctl services ${PLANT_SERVICE} |\
-                awk '/Nameservers.Configuration/ {print}' |\
-                grep -Po '\[\K[^]]*' ${PLANT_NAMESERVERS} | tr -d ',')
-
-       read -e -i "$PLANT_NAMESERVERS" -p 'Nameservers: '
-
-       echo connmanctl config ${PLANT_SERVICE} \
-                       --ipv4 manual ${PLANT_IP} ${PLANT_NETMASK} ${PLANT_GATEWAY} \
-                       --nameservers ${PLANT_NAMESERVERS}
+    2) set_plant_network()
        ;;
 
-    3) echo "Select Config Files: \n\n"
-       echo "Select a collect configuration"
-       # set the prompt used by select, replacing "#?"
-       PS3="Use number to select a file or 'stop' to cancel: "
-
-       # allow the user to choose a file
-       select filename in ./configs/*-collect.yml
-       do
-         # leave the loop if the user says 'stop'
-         if [[ "$REPLY" == stop ]]; then break; fi
-
-         # complain if no file was selected, and loop to ask again
-         if [[ "$filename" == "" ]]; then
-           echo "'$REPLY' is not a valid choice"
-           continue
-         fi
-
-         # now we can use the selected file
-         echo "$filename installed"
-
-         # it'll ask for another unless we leave the loop
-         break
-       done
+    3) select_config_files() 
        ;;
 
     4) echo "Choice 4"
@@ -107,35 +144,9 @@ while true ; do
     5) echo "Choice 5"
        ;;
 
-    *) break
+    0) break
 
   esac
 done
-
-set_post_config () {
-  echo "Select a Post Configuration File"
-  # set the prompt used by select, replacing "#?"
-  PS3="Use number to select a file or 'stop' to cancel: "
-
-  # allow the user to choose a file
-  select filename in ./configs/*-post.yml
-    do
-      # leave the loop if the user says 'stop'
-      if [[ "$REPLY" == stop ]]; then break; fi
-
-      # complain if no file was selected, and loop to ask again
-      if [[ "$filename" == "" ]]
-      then
-        echo "'$REPLY' is not a valid choice"
-        continue
-      fi
-
-      # now we can use the selected file
-      echo "$filename installed"
-
-      # it'll ask for another unless we leave the loop
-    break
-  done
-}
 
 

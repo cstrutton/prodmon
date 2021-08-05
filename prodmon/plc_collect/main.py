e02a4fff44ce01844dbaf9c940ae4c9f653ab484
Chris Strutton <chris@rodandfly.ca>
2021-08-05 13:48:32 -0400

    update state monitor to recognize False as initial read

--- prodmon/plc_collect/main.py
+++ prodmon/plc_collect/main.py
@@ -60,7 +60,7 @@ def loop(config):
             state = 'NoValue'
             if entry['driver'] == 'pylogix':
                 state = read_pylogix_tag(entry)
-                logger.info(f'State: {entry["tag"]}= {state}')
+                logger.debug(f'State: {entry["tag"]}= {state}')
             elif entry['type'] == 'modbus':
                 state = read_modbus_holding(entry)
             if state is not 'NoValue':

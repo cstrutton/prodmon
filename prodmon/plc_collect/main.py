7f5a38c08a346731e1950e7e6dc37cca28ccc63d
Chris Strutton <chris@rodandfly.ca>
2022-07-11 08:53:37 -0400

    update ping sql to use ON DUPLICATE KEY syntax

--- prodmon/plc_collect/main.py
+++ prodmon/plc_collect/main.py
@@ -57,11 +57,17 @@ class PingTag(Tag):
             with open(file_path, "a+") as file:
                 file.write(sql)
 
-    def entry_sql(self, timestamp):
+    def entry_sql2(self, timestamp):
         sql = f'REPLACE INTO {self.dbtable}  '
         sql += f'VALUES ("{self.name}", {timestamp});\n'
         return sql
 
+    def entry_sql(self, timestamp):
+        sql = f'INSERT INTO {self.dbtable}  '
+        sql += f'VALUES ("{self.name}", {timestamp}) ' \
+               f'ON DUPLICATE KEY UPDATE ' \
+               f'Name= "{self.name}", Timestamp= {timestamp};\n'
+        return sql
 
 class CounterTag(Tag):
 
